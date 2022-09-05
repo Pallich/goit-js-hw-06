9dbb428e19f2d3a3c371292bbff6f993f08a5e85
Diachenko Oleksandr <palllich@gmail.com>
2022-09-06 01:44:27 +0300

    make 9 task

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const bodyEl = document.querySelector("body");
+const myEl = document.querySelector(".widget");
+const textEl = document.querySelector(".color");
+
+myEl.addEventListener("click", changeColor);
+
+function changeColor() {
+  bodyEl.style.backgroundColor = getRandomHexColor();
+  textEl.textContent = getRandomHexColor();
+}
