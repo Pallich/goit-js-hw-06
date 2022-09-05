880b22ec9ec5cc0722595c014bfeef91a1e2f058
Diachenko Oleksandr <palllich@gmail.com>
2022-09-06 01:35:41 +0300

    make 8 task

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,40 @@
+const myForm = document.querySelector(".login-form");
 
+myForm.addEventListener("submit", sendForm);
+
+function sendForm(event) {
+  event.preventDefault();
+  const result = {};
+  const emailValue = event.currentTarget.elements.email.value;
+  const passValue = event.currentTarget.elements.password.value;
+
+  if (emailValue == "" || passValue == "") {
+    alert("Слава Україні!");
+  } else {
+    result.email = emailValue;
+    result.password = passValue;
+    event.currentTarget.reset();
+  }
+  console.log(result);
+}
+
+// const myForm = document.querySelector(".login-form");
+
+// myForm.addEventListener("submit", submitForm);
+
+// function submitForm(event) {
+//   event.preventDefault();
+//   const result = {};
+//   const formData = new FormData(event.currentTarget);
+
+//   formData.forEach((value, name) => {
+//     if ((name == "email") & (value != "")) {
+//       result.email = value;
+//       event.currentTarget.reset();
+//     } else if ((name == "password") & (value != "")) {
+//       result.password = value;
+//       event.currentTarget.reset();
+//     } else alert("Слава Україні!");
+//   });
+//   console.log(result);
+// }
